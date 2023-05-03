189a590e8b880006be8979dc3b6ea90faf9cffe4
thang240822 <thang2109vt@gmail.com>
2023-05-03 15:17:23 +0700

    update1

--- phone-store/src/main/java/com/phone/constants/ActiveConstant.java
+++ phone-store/src/main/java/com/phone/constants/ActiveConstant.java
@@ -1,8 +1,8 @@
 package com.phone.constants;
 
 public class ActiveConstant {
-	public static final boolean ENABLE = true; 
-	public static final boolean DISABLE = false; 
+	public static final boolean ENABLE = true; //1
+	public static final boolean DISABLE = false; //0
 	
 	private ActiveConstant() {}
 }
